080378043501c05cb4506286d58c73aa55bace1e
Trevoir Williams <trevoir.williams@gmail.com>
2022-12-08 07:25:43 -0500

    Updated Chapter 05

--- Ch05/HealthCare.Microservices/HealthCare.Appointments.Api/Program.cs
+++ Ch05/HealthCare.Microservices/HealthCare.Appointments.Api/Program.cs
@@ -5,18 +5,20 @@ using HealthCare.Appointments.Api.Repositories;
 using HealthCare.Appointments.Api.Services;
 using MassTransit;
 using HealthCare.Appointments.Api.Configuraitons;
+using System.Reflection;
+using HealthCare.Appointments.Api.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<AppointmentsDbContext>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddHttpClient();
 
-builder.Services.AddMediatR(typeof(Program));
+builder.Services.AddMediatR(Assembly.GetExecutingAssembly());
 builder.Services.AddSingleton<ApiEndpoints>();
 
 builder.Services.AddScoped<IAppointmentRepository, AppointmentRepository>();
